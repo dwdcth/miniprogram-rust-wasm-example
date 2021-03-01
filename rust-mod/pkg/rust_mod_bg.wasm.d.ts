5b1c79ab03716b0300c2d69fc87a5e41a036284e
tanghe <tanghe@ctfo.com>
2021-03-01 10:10:47 +0800

    漏提交js

---
+++ rust-mod/pkg/rust_mod_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function sum(a: number, b: number): number;
+export function hello_world(a: number): void;
+export function md5(a: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
